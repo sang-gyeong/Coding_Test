4464a8fad2ad10a631e8e28b9763508e7b0093b3
sang-gyeong <sknglee22@naver.com>
2021-01-20 17:50:51 +0900

    [20.01.20] 보석 쇼핑

--- lv3/jewerly.py
+++ lv3/jewerly.py
@@ -1,40 +1,43 @@
+from collections import deque
+
 def solution(gems):
     answer = []
     count = 100000
-    gemSet = set(gems)
-    n = len(gemSet)
-    start=0
-    for end in range(1, len(gems)+1):
-        now = set(gems[start:end])
-        if len(set(now)) == n:
-            while len(set(gems[start+1:end])) == n:
-                start += 1
-            gap = end-start+1
-            if count > gap : 
-                count = gap
-                answer = [start+1, end]
-            if n == gap:
-                return answer
-    
-    return answer
-
-from collections import deque
-
-def solution2(gems):
-    answer = []
-    count = 100000
-    gemSet = set(gems)
-    n = len(gemSet)
-    m = len(gems)
+    n = len(set(gems))
+    gem_dict = dict()
     end = 0
-    now = deque()
-    for start in range(m):
-        while set(now) < gemSet and end < m:
-            now.append(gems[end])
+    for start in range(len(gems)):
+        while len(gem_dict) < n and end < len(gems):
+            if gem_dict.get(gems[end]) is None : gem_dict[gems[end]] = 1
+            else : gem_dict[gems[end]] += 1
             end += 1
-        if set(now) == gemSet:
+        if len(gem_dict) == n:
             if count > end-start+1:
                 count = end-start+1
                 answer = [start+1, end]
-        now.popleft()
+        gem_dict[gems[start]] -= 1
+        if gem_dict[gems[start]] == 0 : del gem_dict[gems[start]]
     return answer
+
+
+# 효율성에서 시간초과가 났던 코드
+
+# from collections import deque
+# def solution(gems):
+#    answer = []
+#    count = 100000
+#    gemSet = set(gems)
+#    n = len(gemSet)
+#    m = len(gems)
+#    end = 0
+#    now = deque()
+#    for start in range(m):
+#        while set(now) < gemSet and end < m:
+#            now.append(gems[end])
+#            end += 1
+#        if set(now) == gemSet:
+#            if count > end-start+1:
+#                count = end-start+1
+#                answer = [start+1, end]
+#        now.popleft()
+#    return answer
