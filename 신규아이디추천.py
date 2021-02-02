d81d8cb1ada31e95764814a4f8afc8a4aebcdad7
sang-gyeong <sknglee22@naver.com>
2021-02-02 13:37:44 +0900

    [20.02.02] 신규 아이디 추천 최종제출 코드

--- 신규아이디추천.py
+++ 신규아이디추천.py
@@ -1,28 +1,19 @@
-import re
+import re 
 def solution(new_id):
+    # 1
+    st = new_id.lower()
+    # 2
+    st = re.sub('[^a-z0-9\-_.]+', '', st)
+    # 3
+    st = re.sub('\.{2,}', '.', st)
+    # 4
+    st = re.sub('^\.|\.$', '', st)
+    # 5
+    st = st if len(st) > 0 else 'a'
+    # 6
+    st = st if len(st) < 16 else st[:15]
+    st = re.sub('\.$', '', st)
+    # 7
+    st = st if len(st) > 2 else st + st[-1] * (3-len(st))
     
-
-    p = re.compile('[a-z0-9-_.]+')
-    p2 = re.compile('[^.]+')
-    answer = ''
-    new_id = new_id.lower()
-    new_id = "".join(p.findall(new_id))
-    while ".." in new_id :
-        new_id = re.sub('\.{2}', '.', new_id)
-    if new_id == "." :
-        new_id = ""
-    while new_id != "" and (new_id[0] == '.' or new_id[-1] == '.') :
-        if new_id[0] == '.' :
-            new_id = new_id[1:]
-        elif new_id[-1] == '.' :
-            new_id = new_id[:-1]
-    if new_id == "" :
-        new_id = "a"
-    if len(new_id) >= 16 :
-        new_id = new_id[:15]
-        while new_id != "" and new_id[-1] == '.' :
-            new_id = new_id[:-1]
-    while len(new_id) < 3:
-            new_id += new_id[-1]
-    
-    return new_id
+    return st
